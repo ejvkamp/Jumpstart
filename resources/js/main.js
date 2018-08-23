b3baf9c89c75f62517be0fdb956cebbc1b2b8636
ejvkamp <36860035+ejvkamp@users.noreply.github.com>
2018-08-23 12:03:35 -0700

    Added carousel and slick scrolling in main.js

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+  
+
+});
