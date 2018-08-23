50fe626881494f017c2f4a3a3133262b9fc1c3c8
ejvkamp <36860035+ejvkamp@users.noreply.github.com>
2018-08-23 12:22:34 -0700

    Added scroll function in main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,6 +8,16 @@ $(document).ready(function() {
     pauseOnHover: false
   });
 
-  
-
+  var userScrolled = 0;
+  $(window).scroll(function() {
+    var scrollTopVal = $(this).scrollTop();
+    if (scrollTopVal - userScrolled > 50) {
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      userScrolled = scrollTopVal;
+    } else if (userScrolled - scrollTopVal > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      userScrolled = scrollTopVal;
+    }
+  });
 });
